9cdeafdbad5544a71eac4b55efc0abd90f8f9263
XiNGRZ <chenxingyu92@gmail.com>
2019-06-02 01:19:38 +0800

    audio: Use generated_kernel_includes instead of audio_kernel_headers
    Change-Id: I30fa98078bc8c1e5561516b81ee3582bd31d3475

--- post_proc/Android.mk
+++ post_proc/Android.mk
@@ -73,7 +73,7 @@ LOCAL_C_INCLUDES := \
         $(call include-path-for, audio-effects)
 
 ifeq ($(strip $(AUDIO_FEATURE_ENABLED_DLKM)),true)
-  LOCAL_HEADER_LIBRARIES += audio_kernel_headers
+  LOCAL_HEADER_LIBRARIES += generated_kernel_includes
   LOCAL_C_INCLUDES += $(TARGET_OUT_INTERMEDIATES)/vendor/qcom/opensource/audio-kernel/include
   LOCAL_ADDITIONAL_DEPENDENCIES += $(BOARD_VENDOR_KERNEL_MODULES)
 endif
@@ -179,7 +179,7 @@ LOCAL_C_INCLUDES := \
         external/tinycompress/include
 
 ifeq ($(strip $(AUDIO_FEATURE_ENABLED_DLKM)),true)
-  LOCAL_HEADER_LIBRARIES += audio_kernel_headers
+  LOCAL_HEADER_LIBRARIES += generated_kernel_includes
   LOCAL_C_INCLUDES += $(TARGET_OUT_INTERMEDIATES)/vendor/qcom/opensource/audio-kernel/include
   LOCAL_ADDITIONAL_DEPENDENCIES += $(BOARD_VENDOR_KERNEL_MODULES)
 endif
